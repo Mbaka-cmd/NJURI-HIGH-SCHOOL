7b0c79c0402413e71753fe9c12ab2ccd97fafc5d
Mbaka-cmd <official.mercymbaka@gmail.com>
2026-03-13 09:42:02 +0300

    Clean up duplicate and junk files

--- templates/schools/write_dashboard.py
+++
@@ -1,3 +0,0 @@
-with open('templates/schools/admin_dashboard.html', 'w', encoding='utf-8') as f:
-    f.write(open('write_dashboard_content.html', 'r', encoding='utf-8').read())
-print("Done! File written.")
